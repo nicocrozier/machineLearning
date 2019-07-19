cc572c56d70f85ed4b2e50c7876ab56c962b5dae
Nico C <nicholas@paradisbasics.com>
2019-07-19 01:46:22 -0700

    changed things for iris

--- ApplesOranges/FruitPredict.py
+++ ApplesOranges/FruitPredict.py
@@ -1,7 +1,7 @@
 from sklearn import tree
 
 features = [[140, 1], [130, 1], [150, 0], [170, 0]] # 0=bumpy 1=smooth INPUT
-labels = [0, 0, 1, 1 ]       # 0 = apple     1 = orange   OUTPUT
+labels = [0, 0, "oranges", "oranges" ]       # 0 = apple     1 = orange   OUTPUT
 
 clf = tree.DecisionTreeClassifier()  #classifier
 
---
+++ iris/iris.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+print (iris.feature_names)
+print (iris.target_names)
+print()
+print(iris.data[3])
